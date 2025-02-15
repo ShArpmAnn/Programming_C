e7a0db08ea6a4c50c93ac1ceaa9f343390cad2db
ShArpmAnn <kirc16761@gmail.com>
2025-02-15 13:09:56 +1000

    Add files via upload

--- lab1/main.c
+++ lab1/main.c
@@ -7,16 +7,29 @@ int main(int argc, char *argv[]) {
 	
 	double a, b, c;
 	double x1, x2, d;
-	a = 5.3;
-	b = 1.1;
-	c = 6.0;
+
+	printf("Vvedi a: \n");
+	scanf("%lf", &a);
+	printf("Vvedi b: \n");
+	scanf("%lf", &b);
+	printf("Vvedi c: \n");
+	scanf("%lf", &c);
 	
 	d = b*b - 4*a*c;
-	x1 = (-b + sqrt(d))/(2*a);
-	x2 = (-b - sqrt(d))/(2*a);
-	printf("x1 = %lf\n", x1);
-	printf("x2 = %lf\n", x2);
-	printf("d = %lf\n", d);
+	if (d < 0) printf("KORNEI NET");
+	if (d > 0) {
+		x1 = (-b + sqrt(d))/(2*a);
+		x2 = (-b - sqrt(d))/(2*a);
+		printf("x1 = %lf\n", x1);
+		printf("x2 = %lf\n", x2);
+		printf("d = %lf\n", d);
+		
+	}
+	if (d == 0){
+		
+		x1 = (-b + sqrt(d))/(2*a);
+		printf("ONLY 1 COR x = %lf\n",x1);
+	}
 	
 	return 0;
 }
