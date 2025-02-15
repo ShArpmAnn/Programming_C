0582b3922aede10a4670e2e920478dd03b7fbbf6
ShArpmAnn <kirc16761@gmail.com>
2025-02-15 12:42:29 +1000

    Add files via upload

--- lab1/main.c
+++ lab1/main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int main(int argc, char *argv[]) {
+	
+	double a, b, c;
+	double x1, x2, d;
+	a = 5.3;
+	b = 1.1;
+	c = 6.0;
+	
+	d = b*b - 4*a*c;
+	x1 = (-b + sqrt(d))/(2*a);
+	x2 = (-b - sqrt(d))/(2*a);
+	printf("x1 = %lf\n", x1);
+	printf("x2 = %lf\n", x2);
+	printf("d = %lf\n", d);
+	
+	return 0;
+}
