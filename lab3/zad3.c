c70c0c87a18f68ac661ea4a6cb6f782c48168a73
ShArpmAnn <kirc16761@gmail.com>
2025-02-19 21:22:04 +1000

    Add files via upload

--- lab3/zad3.c
+++ lab3/zad3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char a[80];
+	
+	
+	printf("Vvedi stroku do 80 simvolov: ");
+	fgets(a, 80, stdin);
+	printf("%s", a);
+	
+	
+	
+	return 0;
+}
