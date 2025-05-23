4d6ecc7640a901b0cc28e6e627f322c971e21489
ShArpmAnn <kirc16761@gmail.com>
2025-05-24 00:05:22 +1000

    Add files via upload

--- laba15/zad13.c
+++ laba15/zad13.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void numf(int n) {
+  if (n == 0) return; 
+  numf(n / 10);
+  printf("%d ", n % 10);
+}
+
+void numb(int n) {
+  if (n == 0) return;
+  printf("%d ", n % 10);
+  numb(n / 10);
+}
+
+
+int main(int argc, char *argv[]) {
+	int n;
+	printf("Vvedi chislo ");
+	scanf("%d", &n);
+	numf(n);
+	printf("\n");
+	numb(n);
+	
+	
+	return 0;
+}
